ece19ab505fa29faee14edcb10020f8fcea337da
Tuan <minhtuanapro@gmail.com>
2019-06-14 21:14:41 +0700

    - Patient DoStart

--- PatientAndVirus/Patient.cpp
+++ PatientAndVirus/Patient.cpp
@@ -1,9 +1,11 @@
 #include "Patient.h"
-
+#include "FluVirus.h"
+#include "DengueVirus.h"
 
 Patient::Patient()
 {
 	this->InitResistance();
+	this->DoStart();
 }
 
 
@@ -19,5 +21,22 @@ void Patient::InitResistance()
 void Patient::DoStart()
 {
 	this->m_state = 1;
-
+	std::list<MyVirus> m_virusList(rand() % (20 - 10 + 1) + 10);
+	for (int i = 0; i < m_listVirus.size(); i++)
+	{
+		int n = rand() % (2 - 1 + 1) + 1;
+		switch (n)
+		{
+		case 1:
+			FluVirus *flu = new FluVirus();
+			m_listVirus.push_front(flu);
+			break;
+		case 2:
+			DengueVirus *dengue = new DengueVirus();
+			m_listVirus.push_front(dengue);
+			break;
+		default:
+			break;
+		}
+	}
 }
