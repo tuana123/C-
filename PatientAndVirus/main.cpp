6061c20d03f4d18b0b87e23f74475b8905167157
Tuan <minhtuanapro@gmail.com>
2019-06-15 16:54:19 +0700

    + func main

---
+++ PatientAndVirus/main.cpp
@@ -0,0 +1,22 @@
+//#include "MyVirus.h"
+#include <iostream>
+#include <fstream>
+#include "Patient.h"
+using namespace std;
+void main()
+{
+	Patient *p;
+	char t = 0;
+	while (p->GetState() == 1)
+	{
+		cout << "Take Medicine (0 = NO, 1 = YES";
+		cin >> t;
+		if (t == 1)
+		{
+			int min = 1;
+			int max = 60;
+			int medicine_resistance = min + (rand() % (int)(max - min + 1));
+			p->TakeMedicine(medicine_resistance);
+		}
+	}
+}
